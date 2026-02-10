47989e195872dbd6950f395d883f91f023ffe94e
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-10 19:41:54 +0100

    completed the validate_error function and added a test of user_id

--- 01/orders.py
+++ 01/orders.py
@@ -1,6 +1,26 @@
-def validate_order(order):
+
+def validate_order(order): 
     if not isinstance(order.get("user_id"), int):
         raise ValueError("Nieprawidłowy user_id")
     # uzupełnij resztę przypadków
+    if not order.get("items"):
+        raise ValueError("Pusta lista items")
+    delivery_method = ("courier", "pickup", "drone")
+    if order["delivery"]["method"] not in delivery_method:
+        raise ValueError("Nieprawidłowy sposób dostawy")
+    if not order["delivery"]["address"]:
+        raise ValueError("Brak adresu")
 
     return True
+
+
+if __name__ == "__main__":
+    order = {
+    "user_id": 101,
+    "items": ["keyboard", "mouse", "monitor"],
+    "delivery": {
+        "method": "drone",         
+        "address": "ul. Polna 10, 00-001 Warszawa"
+        }
+    }
+    print(validate_order(order=order))
--- 01/test_orders.py
+++ 01/test_orders.py
@@ -2,7 +2,22 @@ import unittest
 from orders import validate_order
 
 class TestValidateOrder(unittest.TestCase):
-   pass
+   def test_if_user_id_is_correct(self):
+      order = {
+         "user_id": "",
+         "items": ["keyboard", "mouse", "monitor"],
+         "delivery": {
+            "method": "courier",         
+            "address": "ul. Polna 10, 00-001 Warszawa"
+            }
+         }
+      with self.assertRaises(ValueError) as context:
+         validate_order(order)
+      
+      self.assertEqual(str(context.exception), "Nieprawidłowy user_id")
+
+   
+   
 
 if __name__ == "__main__":
     unittest.main()
