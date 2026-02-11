2e72075aec154ba23a5d276e8d95131931bd0038
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-11 06:59:06 +0100

    task-01 completed

--- 01/test_orders.py
+++ 01/test_orders.py
@@ -15,6 +15,51 @@ class TestValidateOrder(unittest.TestCase):
          validate_order(order)
       
       self.assertEqual(str(context.exception), "Nieprawidłowy user_id")
+   
+   
+   def test_items_in_orders_are_empty(self):
+      order = {
+         "user_id": 10,
+         "items": [],
+         "delivery": {
+            "method": "courier",         
+            "address": "ul. Polna 10, 00-001 Warszawa"
+            }
+         }
+      with self.assertRaises(ValueError) as context:
+         validate_order(order)
+      
+      self.assertEqual(str(context.exception), "Pusta lista items")
+    
+      
+   def test_incorrect_delivery_method(self):
+      order = { 
+         "user_id": 10,
+         "items": ["keyboard", "mouse", "monitor"],
+         "delivery": {
+            "method": "test",         
+            "address": "ul. Polna 10, 00-001 Warszawa"
+            }
+         }
+      with self.assertRaises(ValueError) as context:
+         validate_order(order)
+   
+      self.assertEqual(str(context.exception), "Nieprawidłowy sposób dostawy")
+               
+   
+   def test_empty_delivery_address(self):
+      order = {
+         "user_id": 10,
+         "items": ["keyboard", "mouse", "monitor"],
+         "delivery": {
+            "method": "drone",         
+            "address": ""
+            }
+         }
+      with self.assertRaises(ValueError) as context:
+         validate_order(order)
+      
+      self.assertEqual(str(context.exception), "Brak adresu")
 
    
    
