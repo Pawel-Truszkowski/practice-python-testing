874bb85b681e616f3989553fa7157b5b12949c32
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-14 09:43:48 +0100

    task-02 completed

--- 02/promo.py
+++ 02/promo.py
@@ -1,5 +1,16 @@
 import string
 
 def is_valid_promo(code):
+    if not isinstance(code, str):
+        return False
+    if len(code) < 10 or len(code) > 10:
+        return False
+    if any(char.islower() for char in code):
+        return False
+    if len([char for char in code if char.isdigit()]) < 2:
+        return False
+    if not code.isalnum():
+        return False
     
     return True
+
