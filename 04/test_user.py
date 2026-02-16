ea44f800a8059a6ba389e38072576b4d0ae35347
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-16 20:58:06 +0100

    Completed task-04

--- 04/test_user.py
+++ 04/test_user.py
@@ -2,5 +2,32 @@ import pytest
 from user import validate_user
 
 
+@pytest.fixture
 def valid_user_data():
-    pass
+    return {
+        "name": "Test",
+        "email": "test@testcase.com",
+        "age": 20,
+    }
+
+
+def test_validate_user_with_valid_data(valid_user_data):
+    result = validate_user(valid_user_data)
+    assert result == True
+    
+
+def test_validate_suer_with_invalid_email(valid_user_data):
+    valid_user_data['email'] = 'testcase.com'
+    result = validate_user(valid_user_data)
+    assert result == False
+    
+    
+def test_validate_suer_with_invalid_name(valid_user_data):
+    valid_user_data['name'] = 'Test10?!'
+    result = validate_user(valid_user_data)
+    assert result == False
+
+def test_validate_suer_with_invalid_age(valid_user_data):
+    valid_user_data['age'] = 16
+    result = validate_user(valid_user_data)
+    assert result == False
--- 04/user.py
+++ 04/user.py
@@ -1,3 +1,16 @@
 def validate_user(data):
+    char = "@"
+    if char not in data['email']:
+        return False
+    
+    if not data['email'].endswith((".pl", ".com")):
+        return False
+    
+    if isinstance(data['age'], int) and data['age'] < 18:
+        return False
+        
+    if not data['name'].isalpha():
+        return False
     
     return True
+
