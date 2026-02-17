f4ac74918b7620b95bfdbff55adac2b68b53c283
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-17 21:27:30 +0100

    Completed task-05

--- 05/cart.py
+++ 05/cart.py
@@ -1,2 +1,21 @@
 class Cart:
-    pass
+    def __init__(self):
+        self._products = []
+           
+    def add(self, product):
+        self._products.append(product)
+    
+    def remove(self, product):
+        for index, value in enumerate(self._products):
+            if value.get("name") == product:
+                del self._products[index]
+                return True
+        return False
+                
+    def total(self):
+        return sum(p['price'] for p in self._products)
+      
+    def items(self):
+        return [product.copy() for product in self._products]
+    
+
--- 05/test_cart.py
+++ 05/test_cart.py
@@ -1,17 +1,51 @@
 import pytest
 from cart import Cart
 
-def test_add_product():
-    pass
 
-def test_remove_product():
-    pass
+@pytest.fixture
+def products():
+    return [
+        {"name": "apple", "price": 3.5},
+        {"name": "banana", "price": 3.5},
+        {"name": "apple", "price": 4.2}
+    ]
+    
 
-def test_total_price():
-    pass
+def test_add_product(products):
+    cart = Cart()
+    cart.add(products[0])
+    assert cart._products == [{"name": "apple", "price": 3.5}]
+    cart.add(products[1])
+    assert cart._products == [{"name": "apple", "price": 3.5}, {"name": "banana", "price": 3.5}]
+    
 
+
+def test_remove_product(products):
+    cart = Cart()
+    for product in products: cart.add(product)
+    result = cart.remove("apple")
+    assert cart._products == [
+        {"name": "banana", "price": 3.5},
+        {"name": "apple", "price": 4.2}
+    ]
+    assert result == True
+
+
+def test_total_price(products):
+    cart = Cart()
+    for product in products: cart.add(product)
+    assert cart.total() == 11.2
+ 
+    
 def test_empty_cart_total():
-    pass
+    cart = Cart()
+    assert cart.total() == 0
 
-def test_remove_nonexistent_product():
-    pass
+
+def test_remove_nonexistent_product(products):
+    cart = Cart()
+    for product in products: cart.add(product)
+    result = cart.remove("nonexistent")
+    assert cart._products == products
+    assert result == False
+    
