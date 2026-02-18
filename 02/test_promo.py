1c1f5ebd17e013f06a4b49f6afaaa4b14b5cb78f
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-18 20:12:02 +0100

    Corrected task-02 - test cases added

--- 02/test_promo.py
+++ 02/test_promo.py
@@ -5,10 +5,13 @@ from promo import is_valid_promo
 @pytest.mark.parametrize("code, expected", [
     ("ABC123XY90", True),
     ("AB123", False),
+    ("ABCDEFGHIJ1234", False),
     ("abc123defg", False),
     ("ABCDEFGHIJ", False),
     (1234567890, False),
     ("ABC!@#123$", False),
+    ("ABCDEFGH12", True),
+    ("ABCDEFGHJ1", False),
 ])
 def test_is_valid_promo(code, expected):
     assert is_valid_promo(code) == expected
